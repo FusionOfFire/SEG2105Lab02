47c4c4dcd35e317dc0ec09bc7be99fbef542dbd6
Frederick Gaudet <fgaud013@uottawa.ca>
2018-09-29 13:28:07 -0400

    Adding some change to LoadTesting

--- LoadTesting.java
+++ LoadTesting.java
@@ -16,44 +16,29 @@ public class LoadTesting
 	
 	public static void main(String[] args)
 	{
-		testDesignThreeAndTwo();
-		//testDesignSix();
+		int nbTry = 10000000;
+		System.out.println("\n================ DESIGN 2 ==================\n");
+		testDesignTwo(nbTry);//Testing design 2
+		System.out.println("\n================ DESIGN 3 ==================\n");
+		testDesignThree(nbTry);
+		System.out.println("\n================ DESIGN 6 ==================\n");
+		testDesignSix(nbTry);
 		
 	}
-	
 	/**
-	 * Test for design 2, 3. On regular array
+	 * Test for design 3. On regular array
 	 */
-	public static void testDesignThreeAndTwo()
+	public static void testDesignTwo(int nbTest)
 	{
 		/* Nb of test we want to try
 		 * 
 		 */
-		//int nbTest = 30000000;//Best test simulation to get around 9 secondes
-		int nbTest = 1000;
 		long startTime = 0;
 		long endTime = 0;
 		float result = 0;
 		
-		System.out.println("Doing the design 2 & 3 testing on a regular array.");
-		//Generating the cartesian
-		PointCart[] pc = new PointCart[nbTest];
-		
-		System.out.println("Generating \t: " + nbTest + " cartesian coordinates...");
-		startTime = System.currentTimeMillis();
-		
-		for(int i=0; i<nbTest;i++)
-		{
-			double x = (Math.random() * 1000);
-			double y = (Math.random() * 1000);
+		System.out.println("Doing the design 2 (Polar) testing on a regular array.\n");
 			
-			pc[i] = new PointCart('C',x,y);
-		}
-
-		endTime = System.currentTimeMillis();
-		result = (endTime - startTime)/1000;
-		System.out.println("Time \t\t: " + result + " secondes for the cartesian coordinate.");
-		
 		
 		//Generating the polar
 		PointPolar[] pp = new PointPolar[nbTest];
@@ -76,15 +61,42 @@ public class LoadTesting
 	}
 	
 	/**
+	 * Test for design 3. On regular array
+	 */
+	public static void testDesignThree(int nbTest)
+	{
+
+		long startTime = 0;
+		long endTime = 0;
+		float result = 0;
+		
+		System.out.println("Doing the design 3 (Cartesian) testing on a regular array.");
+		//Generating the cartesian
+		PointCart[] pc = new PointCart[nbTest];
+		
+		System.out.println("Generating \t: " + nbTest + " cartesian coordinates...");
+		startTime = System.currentTimeMillis();
+		
+		for(int i=0; i<nbTest;i++)
+		{
+			double x = (Math.random() * 1000);
+			double y = (Math.random() * 1000);
+			
+			pc[i] = new PointCart('C',x,y);
+		}
+
+		endTime = System.currentTimeMillis();
+		result = (endTime - startTime)/1000;
+		System.out.println("Time \t\t: " + result + " secondes for the cartesian coordinate.");
+		
+	}
+	
+	/**
 	 * Test for design 6, testing with interface
 	 */
-	public static void testDesignSix()
+	public static void testDesignSix(int nbTest)
 	{
-		/* Nb of test we want to try
-		 * 
-		 */
-		//int nbTest = 30000000;//Best test simulation to get around 9 secondes
-		int nbTest = 1000;
+
 		long startTime = 0;
 		long endTime = 0;
 		float result = 0;
